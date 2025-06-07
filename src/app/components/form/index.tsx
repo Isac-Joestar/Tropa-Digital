21afd5fd748b5f0bb3deef3a55021ff3fb5855e0
Isac Mikael <109987080+Isac-Joestar@users.noreply.github.com>
2025-06-06 22:35:18 -0300

    responsividade

--- src/app/components/form/index.tsx
+++ src/app/components/form/index.tsx
@@ -2,20 +2,39 @@
 
 import { useState } from "react";
 import styles from './index.module.css'
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 function Form (){
+    const router = useRouter();
     const [showPassword, setShowPassword] = useState(false);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+    
+        if (email && password) {
+          router.push("/events"); 
+        } else {
+          alert("Preencha todos os campos.");
+        }
+      };
     return(
-        <form action="" className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
             <div className={styles.content_input}>
                 <label htmlFor="email" className={styles.label}>E-mail</label>
-                <input type="email" name="email" className={styles.input} id="email" placeholder="seunome@seuservidor.com"/>
+                <input type="email" name="email" id="email" className={styles.input} placeholder="seunome@seuservidor.com"/>
             </div>
             <div className={styles.content_input}>
                 <label htmlFor="password" className={styles.label}>Senha</label>
                 <div className={styles.passwordWrapper}>
                     <input
                         type={showPassword ? "text" : "password"}
+                        name="password"
                         className={styles.input}
                         id="password"
                         placeholder="Digite aqui"
@@ -29,7 +48,7 @@ function Form (){
                     </button>
                 </div>
             </div>
-            <button className={styles.button}>Enviar</button>
+            <button type="submit" className={styles.button}>Enviar</button>
         </form>
     )
 }
